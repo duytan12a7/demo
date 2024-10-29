12249e83e5ed2b3f3585d7f727fdc589e2723a93
duytan12a7 <101546603+duytan12a7@users.noreply.github.com>
2024-10-29 23:13:49 +0700

    Update counttime.js

--- js/counttime.js
+++ js/counttime.js
@@ -1,4 +1,5 @@
-const yourDate = new Date("2022-12-25T22:22:22");
+const yourDate = new Date("2022-05-19T22:22:22");
+//const yourDate = new Date("2022-12-25T22:22:22");
 // const yourDate = new Date("2022-12-31T22:22:22");
 // const yourDate = new Date("2023-01-14T21:15:00");
 // const yourDate = new Date("2023-02-01T21:15:00");
